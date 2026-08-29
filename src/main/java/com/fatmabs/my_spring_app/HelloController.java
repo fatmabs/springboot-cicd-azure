2c20eb2ae13d16967f16068b2dae44998602866f
Fatma <fatmabenslim4t1@gmail.com>
2026-08-29 21:53:08 +0100

    Update Spring Boot welcome message

--- src/main/java/com/fatmabs/my_spring_app/HelloController.java
+++ src/main/java/com/fatmabs/my_spring_app/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello from Spring Boot App!";
+        return "Hello from Spring Boot App! nice to meet you";
     }
 }
