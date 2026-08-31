f9f596794a5b2195e25b0f8f6c6f23c34e6c8ce8
Fatma <fatmabenslim4t1@gmail.com>
2026-08-31 12:23:29 +0100

    trigger workflow

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,10 +6,12 @@ variable "environment" {
 variable "location" {
   description = "The location of the resources"
   type        = string
+
+  
 }
 
 variable "resource_group_name" {
   description = "The name of the resource group"
-  
+
   type        = string 
 }
